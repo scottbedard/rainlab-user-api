2b6a21b01550ff8c938a53aa8e1c7f4a64978309
Scott Bedard <scottbedard90@gmail.com>
2019-11-24 12:25:05 -0700

    add test for api controller extension

---
+++ tests/unit/classes/ApiControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bedard\RainLabUserApi\Tests\Unit\Classes;
+
+use Bedard\RainLabUserApi\Classes\ApiController;
+use Bedard\RainLabUserApi\Tests\PluginTestCase;
+
+class ApiControllerTest extends PluginTestCase
+{
+    public function test_extending_the_api_controller_with_middleware()
+    {
+        ApiController::extend(function ($controller) {
+            $controller->middleware(TestMiddleware::class);
+        });
+
+        $response = $this->get('/api/rainlab/user/auth/logout');
+        
+        $this->assertEquals('Hello from the test middleware!', $response->getContent());
+    }
+}
+
+class TestMiddleware
+{
+    public function handle($request, $next)
+    {
+        $response = $next($request);
+        $response->setContent('Hello from the test middleware!');
+
+        return $response;
+    }
+}
