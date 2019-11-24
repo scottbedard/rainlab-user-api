d399e9cac7b24c0edfe84c8447b5ce9221ed27ab
Scott Bedard <scottbedard90@gmail.com>
2019-11-24 12:41:43 -0700

    clean up 0.x routes

--- routes.php
+++ routes.php
@@ -29,23 +29,3 @@ if (config('bedard.rainlabuserapi::apiEnable')) {
         });
     });
 }
-
-// Route::prefix('api/rainlab/user')->middleware('web')->group(function() {
-
-//     // // public routes
-//     // Route::any('signout', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@signout');
-//     // Route::any('stop-impersonating', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@stopImpersonating');
-//     // Route::get('activate', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@activate');
-//     // Route::get('reset-password', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@getUserByResetCode');
-//     // Route::post('register', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@register');
-//     // Route::post('reset-password', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@resetPassword');
-//     // Route::post('send-reset-email', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@sendResetEmail');
-//     // Route::post('signin', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@authenticate');
-
-//     // // authenticated routes
-//     // Route::group(['middleware' => 'RainLab\User\Classes\AuthMiddleware'], function () {
-//     //     Route::get('user', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@user');
-//     //     Route::post('user', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@update');
-//     //     Route::delete('user/avatar', 'Bedard\RainLabUserApi\Http\Controllers\AuthController@deleteAvatar');
-//     // });
-// });
